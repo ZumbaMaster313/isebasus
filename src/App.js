86d872b18b5f62c05449ae894a11bb116608f151
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-03-31 19:54:50 -0700

    🔪 umph yas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { forwardRef, useRef, useImperativeHandle, Component} from 'react';
 import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import qwertyVideo from '../src/videos/qwertyVideo.mp4'
+import scraperVideo from '../src/videos/scraperVideo.mp4'
 import Project from './components/projects.jsx';
 import Description from './components/description.jsx';
 import { createBrowserHistory } from 'history';
@@ -19,7 +21,7 @@ function App() {
           <Route path={"/qwerty"} component={Qwerty}></Route>
           <Route path={"/videoscraper"} component={VideoScraper}></Route>
           <Route path={"/rapter"} component={Rapter}></Route>
-          <Route path={"/composte"} component={Composter}></Route>
+          <Route path={"/composter"} component={Composter}></Route>
         </Switch>
       </div>
     </Router>
@@ -55,13 +57,13 @@ class Home extends React.Component{
           <a id="link">about</a>
         </nav>
         <div className="projects">
-          <h1 className="first"><a id="text">hi, i'm sebastian</a> 👋</h1>
-          <h2 className="caption">These are the projects I've developed. Check out my <a id="git" href="https://github.com/ZumbaMaster313/">github</a> &amp; <a id="res">resume</a> for more.</h2>
+          <h1 className="first"><a id="text">hey, i'm sebastian.</a></h1>
+          <h2 className="caption">These are the projects I've developed. Check out my <a id="git" href="https://github.com/ZumbaMaster313/">github</a> &amp; <a id="res">resume</a> for more. </h2>
           <div className="columns">
             <Project projectId="qwerty" path={this.qwerty} name="Qwerty" imagePath={qwerty}></Project>
             <Project projectId="composter" path={this.composter} name="Composter"></Project>
             <Project projectId="videoScraper" path={this.videoScraper} name="Video Scraper"></Project>
-            <Project projectId="rapter" path={this.rapter} name="Rapter"></Project>
+            <Project projectId="pacman" path={this.rapter} name="Pacman"></Project>
           </div>
         </div>
       </header>
@@ -72,21 +74,41 @@ class Home extends React.Component{
 export class Qwerty extends React.Component{
   render(){
     return(
-      <Description></Description>
+      <Description 
+      video={qwertyVideo} 
+      projectName="Qwerty"
+      caption="web proxy"
+      p1="This Web Proxy was originally created for the necessity of searching up websites under my high school wifi. As a result, most websites under the school wifi are blocked so a web proxy would save the day. Until, of course, one day it would finally become a blocked website as well."
+      p2="This project was created with the Flask web server. This application works by HTTP requesting a website to grab its HTML under a given search string. As a result, once the application has the HTMl it could use the css and js links to fully render the site. It does this by integrating this servers domain in front of the css and js links in the HTML so that way it could request the css and js from my server. It of course does not work for ever single website, but it still needs work to be done."
+      p3="Overall, this project was a pain at first, trying to figure out how to render the HTML and css, but it was really fun in my opinion. Check out the code on my GitHub and also check out my website! Links below."
+      >
+      </Description>
     )
   }
 }
 
 export class VideoScraper extends React.Component{
-
+  render(){
+    return(
+      <Description video={scraperVideo} projectName="Video Scraper"></Description>
+    )
+  }
 }
 
 export class Rapter extends React.Component{
-
+  render(){
+    return(
+      <Description projectName="Rapter"></Description>
+    )
+  }
 }
 
 export class Composter extends React.Component{
-
+  render(){
+    return(
+      <Description projectName="Composter"></Description>
+    )
+  }
 }
 
 export default App;
--- src/components/description.jsx
+++ src/components/description.jsx
@@ -5,17 +5,54 @@ import PropTypes from "prop-types";
 
 
 const styles = {
-   
-
-
+    description: {
+        color: 'rgb(255, 255, 255, 0.8)',
+        textAlign: 'left',
+        fontWeight: 'normal',
+        fontSize: '1.5rem',
+        lineHeight: '50px',
+        marginTop: '30px'
+    },
+    link: {
+        cursor: 'pointer',
+        background: 'linear-gradient(to right, #0cebeb, #20e3b2, #29ffc6)',
+        color: '#F1F1F1',
+        borderRadius: '7px',
+        border: '0px', 
+        fontWeight: 700,
+        userSelect: 'none',
+        fontSize: '15px',
+        textDecoration: 'none',
+        letterSpacing: '2px',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        float: 'left',
+        padding: '1rem 1.0rem',
+        fontSize: '13px',
+        boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.1)',
+    },
 };
 
 
-const Container = ({}) =>  {
+const Container = ({projectName, video, p1, p2, p3, caption, gitLink}) =>  {
 
     return(
         <div class="body">
-            <div class="projects"></div>
+            <div class="projects">
+                <div className="links">
+                    <a className="li" href="/">&larr; Back</a>
+                </div>
+                <h1 className="first" id="title">{projectName}</h1>
+                <h2 className="caption" id="pCaption">{caption}</h2>
+                <div>
+                    <video src={video} width="100%" height="auto" preload="auto" muted="true" title="Qwerty" autoplay="true" loop="true"></video>
+                </div>
+                <h3 style = {styles.description} className="projectDescription">{p1}</h3>
+                <h3 style = {styles.description} className="projectDescription">{p2}</h3>
+                <h3 style = {styles.description} className="projectDescription">{p3}</h3>
+                <h3 style = {styles.description} className="projectDescription"></h3>
+                <h3 style = {styles.description} className="projectDescription"></h3>
+            </div>
         </div>
     )
 };
